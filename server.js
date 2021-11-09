9fd6e2adcc7b9a228c7f9d8a663f4ee14c0b037f
Iban Lopez <ibanlopezd@gmail.com>
2021-11-09 18:02:38 +0100

    Basic server setup

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ const app = express()
 
 const PORT  = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`------// SERVER UP AND RUNNING // --- \n - PORT: ${PORT}`))
+app.listen(PORT, () => 
+    console.log(`------// SERVER UP AND RUNNING // --- \n 
+        - PORT: ${PORT} \n 
+        - MODE: ${process.env.NODE_ENV} \n
+        - EXPRESS: ${express ? 'YES' : 'NO'} \n
+        - DB: database `
+))
